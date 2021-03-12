7431792971381b720d8a76c66eab895e7662bf9d
Julio Cesar Bonon <juliocesarbonon@gmail.com>
2021-03-12 10:53:40 -0300

    Add lib/rootPage.dart

---
+++ promoscrapperUI/lib/rootPage.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'auth.dart';
+import 'homePage.dart';
+import 'login.dart';
+
+class RootPage extends StatefulWidget {
+  RootPage({this.auth});
+  final BaseAuth auth;
+  @override
+  _RootPageState createState() => _RootPageState();
+}
+
+enum AuthStatus { notSignedIn, signedIn }
+
+class _RootPageState extends State<RootPage> {
+  AuthStatus authStatus = AuthStatus.notSignedIn;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.auth.currentUser().then((userId) {
+      setState(() {
+        authStatus =
+            userId == null ? AuthStatus.notSignedIn : AuthStatus.signedIn;
+      });
+    });
+  }
+
+  void _signedIn() {
+    setState(() {
+      authStatus = AuthStatus.signedIn;
+    });
+  }
+
+  void _signedOut() {
+    setState(() {
+      authStatus = AuthStatus.notSignedIn;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    switch (authStatus) {
+      case AuthStatus.notSignedIn:
+        return new Login(
+          auth: widget.auth,
+          onSignedIn: _signedIn,
+        );
+      case AuthStatus.signedIn:
+        return new HomePage(
+          auth: widget.auth,
+          onSignedOut: _signedOut,
+        );
+    }
+  }
+}
