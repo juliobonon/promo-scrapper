8ba91ddf7f5b18033b80ea535c0401c783a68cba
Julio Cesar Bonon <juliocesarbonon@gmail.com>
2021-04-15 12:19:47 -0300

    Update database.dart: Add setEntry and getFavoriteProducts methods

--- promoscrapperUI/lib/services/database.dart
+++ promoscrapperUI/lib/services/database.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:promoscrapperUI/models/favoriteProduct.dart';
 import 'package:promoscrapperUI/models/product.dart';
 
 class DatabaseService {
@@ -6,6 +7,20 @@ class DatabaseService {
 
   Stream<List<Product>> getProducts() {
     return _db.collection('promos').snapshots().map((snapshot) =>
-        snapshot.docs.map((doc) => Product.createMap(doc.data())).toList());
+        snapshot.docs.map((doc) => Product.fromJson(doc.data())).toList());
+  }
+
+  Stream<List<Product>> getFavoriteProducts() {
+    return _db.collection('favorites').snapshots().map((snapshot) =>
+        snapshot.docs.map((doc) => Product.fromJson(doc.data())).toList());
+  }
+
+  Future<void> setEntry(FavoriteProduct favoriteProduct) {
+    var options = SetOptions(merge: true);
+
+    return _db
+        .collection('favoriteProducts')
+        .doc(favoriteProduct.id)
+        .set(favoriteProduct.toMap(), options);
   }
 }
